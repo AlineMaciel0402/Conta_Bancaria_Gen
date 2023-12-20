e209dd6a6783508d3aebb2abe6feb1349e9aa4fe
AlineMaciel0402 <alinemaciel04@gmail.com>
2023-12-20 17:33:13 -0300

    Conta Bancária

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -238,8 +238,8 @@ public class Menu {
 	public static void sobre() {
 		System.out.println("\n*********************************************************");
 		System.out.println("Projeto Desenvolvido por: ");
-		System.out.println("Generation Brasil - generation@generation.org");
-		System.out.println("github.com/conteudoGeneration");
+		System.out.println("Aline Maciel - alinemaciel04@gmail.com");
+		System.out.println("github.com/AlineMaciel0402/Conta_Bancaria_Gen.git");
 		System.out.println("*********************************************************");
 	}
 
